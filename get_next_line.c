ba4e5a42bec11f9cee667e144ee22482cfcf6765
Jesus Mellado Ramos <jmellado@c1r2s5.42malaga.com>
2025-08-21 12:04:26 +0200

    entregado

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jmellado <jmellado@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 09:08:27 by jmellado          #+#    #+#             */
-/*   Updated: 2025/05/03 19:45:20 by jmellado         ###   ########.fr       */
+/*   Updated: 2025/04/30 20:49:41 by jmellado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,20 +28,20 @@ char	*ft_next(char *buffer)
 	char	*line;
 
 	i = 0;
-	while (buffer[i] && buffer[i] != '\n') // mientras exista un dato en buffer y no sea un salto de linea iteramos
+	while (buffer[i] && buffer[i] != '\n')
 		i++;
-	if (!buffer[i])    // si no existe dato (caso eof)
+	if (!buffer[i])
 	{
 		free(buffer);
 		return (NULL);
 	}
-	line = ft_calloc((ft_strlen(buffer) - i + 1), sizeof(char)); // reservamos espacio para la longuitud de buffer - la linea contando null
+	line = ft_calloc((ft_strlen(buffer) - i + 1), sizeof(char));
 	i++;
 	j = 0;
-	while (buffer[i]) // mientras exista algo en buffer
-		line[j++] = buffer[i++]; // copiamos los caracteres restantes de buffer en line
+	while (buffer[i])
+		line[j++] = buffer[i++];
 	free(buffer);
-	return (line); // devolvemos todos los datos restantes
+	return (line);
 }
 
 char	*ft_line(char *buffer)
@@ -52,18 +52,18 @@ char	*ft_line(char *buffer)
 	i = 0;
 	if (!buffer[i])
 		return (NULL);
-	while (buffer[i] && buffer[i] != '\n') //mientras exista un dato en buffer y no sea un salto de pagina
+	while (buffer[i] && buffer[i] != '\n')
 		i++;
-	line = ft_calloc(i + 2, sizeof(char)); // reservamos el espacio para la linea completa
-	i = 0;								   // reiniciamos el iterador
-	while (buffer[i] && buffer[i] != '\n') // mientras esxista un dato en buffer y no sea salto de linea
+	line = ft_calloc(i + 2, sizeof(char));
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
 	{
-		line[i] = buffer[i];               // almacenamos la linea en line
+		line[i] = buffer[i];
 		i++;
 	}
-	if (buffer[i] && buffer[i] == '\n')    // si hemos llegado al salto de linea
-		line[i++] = '\n';                  //ponemos el salto de linea en line y salimos del bucle despues
-	return (line);                         // devolvemos una linea completa
+	if (buffer[i] && buffer[i] == '\n')
+		line[i++] = '\n';
+	return (line);
 }
 
 char	*read_file(int fd, char *res)
@@ -72,26 +72,26 @@ char	*read_file(int fd, char *res)
 	int		byte_read;
 
 	if (!res)
-		res = ft_calloc(1, 1); // Inicio el espacio de memoria para el resto
-	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char)); // inicio el espacio de memoria para el contenido del txt
-	byte_read = 1; //flag para que empiece la primera ejecucion
-	while (byte_read > 0) //Mientras que tengamos algo leido
+		res = ft_calloc(1, 1);
+	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	byte_read = 1;
+	while (byte_read > 0)
 	{
-		byte_read = read(fd, buffer, BUFFER_SIZE); // lee el fd guardalo en buffer la cantidad de buffer_size (tiene un puntero que se va moviendo cada vez que se llama)
-		if (byte_read == -1) // si el read fallaa
+		byte_read = read(fd, buffer, BUFFER_SIZE);
+		if (byte_read == -1)
 		{
 			free(res);
 			free(buffer);
 			return (NULL);
 		}
-		buffer[byte_read] = 0; // en buffer cantidad total de datos leidos pon un null
-		res = ft_free(res, buffer); // en res guardamos buffer
-		if (ft_strchr(buffer, '\n')) // si en buffer hay un salto de linea paramos el while
+		buffer[byte_read] = 0;
+		res = ft_free(res, buffer);
+		if (ft_strchr(buffer, '\n'))
 			break ;
-		if (byte_read < BUFFER_SIZE) // si lop que hemos leido es menor que el maximo que leemos de golpe (significa que es EOF)
-			return (free(buffer), res); //libera buffer y devuelve ress
+		if (byte_read < BUFFER_SIZE)
+			return (free(buffer), res);
 	}
-	free(buffer); // Si nos salimos del while liberamos buffer
+	free(buffer);
 	return (res);
 }
 
@@ -102,11 +102,11 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	buffer = read_file(fd, buffer); //buffer es los datos leidos
+	buffer = read_file(fd, buffer);
 	if (!buffer)
 		return (NULL);
-	line = ft_line(buffer);  // Line contiene una unica liena completa
-	buffer = ft_next(buffer); // buffer tiene todo lo que no es line guardado de forma estatica, ya line no existe dentro de buffer
+	line = ft_line(buffer);
+	buffer = ft_next(buffer);
 	return (line);
 }
 /*
